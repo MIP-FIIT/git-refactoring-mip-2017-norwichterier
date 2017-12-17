6e4851ed421412707bdd3887f75735fad824db6d
Juraj Mecir <jurajmecir@gmail.com>
2017-12-17 16:47:16 +0100

    odstranenie copypaste,zbytocnych komentarov,prikazov a premennych

--- main.cpp
+++ main.cpp
@@ -3,17 +3,37 @@
 #include <stdio.h>
 #include <string.h>
 
-FILE* vypis(FILE *subor)																							//pointer na txt subor, ktory je v zlozke
+int zrataj_riadky(FILE*subor)
+{
+	int pocet_riadkov=1;
+	char znak;
+	rewind (subor);
+	do
+	{
+		
+		do
+		{
+			
+			znak =getc(subor);
+			if(znak ==EOF)  
+				break;             
+		}while (znak!=('\n'));
+		pocet_riadkov++;
+	}while (znak!=EOF);
+	return(pocet_riadkov);
+}
+
+FILE* vypis(FILE *subor)																						
 {
 	char znak;
-	int cisrad=1;																								// cislo riadku
+	int cislo_riadku=1;																								
 	if((subor = fopen("file.txt", "r")) ==NULL)
 		printf("NEOTVORENY SUBOR\n");
 	else 
 	{
 		do
 		{
-			switch (cisrad%6) 	
+			switch (cislo_riadku%6) 	
 			{
 				case 1:printf("meno priezvisko: ");break;
 				case 2:printf("SPZ: ");break;
@@ -30,7 +50,7 @@ FILE* vypis(FILE *subor)																							//pointer na txt subor, ktory je 
 				if(znak ==EOF)  
 					break;             
 			}while (znak!=('\n')) ;
-			cisrad++;
+			cislo_riadku++;
 		}while (znak!=EOF);
 		rewind (subor);
 	}
@@ -43,28 +63,16 @@ void odmeny(FILE *subor)
 	char znak;
 	int cislo_riadku=1,cislo_zaznamu;
 	int *typ_auta;																// toto pole obsahuje informaciu o type auta daneho zaznamu neskor bude obsahovat hodnoty 1/2/0 podla kt program vyrata,aku odmenu zakaznik dostane
-	long int zadany_datum,datum_zo_zaznamu,pocet_zaznamov;
+	long int zadany_datum,datum_zo_zaznamu,pocet_zaznamov,pocet_riadkov;
 	float odmena;
 	
 	scanf("%li",&zadany_datum);	
-	do
-	{
-		
-		do
-		{
-			
-			znak =getc(subor);
-			if(znak ==EOF)  
-				break;             
-		}while (znak!=('\n'));
-		pocet_zaznamov++;
-	}while (znak!=EOF);
-	
-	pocet_zaznamov=pocet_zaznamov/6;
-	typ_auta=(int*) malloc(sizeof(pocet_zaznamov));
-	rewind(subor);
+	pocet_riadkov=zrataj_riadky(subor)	;
+	pocet_zaznamov=pocet_riadkov/6;
+	typ_auta=(int*) malloc(sizeof(pocet_zaznamov));	
 	cislo_riadku=1;
-	
+	rewind(subor);
+
 	do
 	{
 		
@@ -190,21 +198,13 @@ char* nacitaj(FILE *subor)
 	int riadok,poradie_znaku,pocet_riadkov,velkost_pola; 																
 	char *pole_SPZ;	
 			
-	rewind(subor);
 	if (sizeof(*pole_SPZ)!=1)
 		free(pole_SPZ);
-	do
-	{
-		do
-		{
-			znak=getc(subor);
-			if(znak ==EOF)  
-				break;             
-		}while (znak!=('\n')) ;
-		pocet_riadkov++;
-	}while (znak!=EOF);	
+	
+	pocet_riadkov=zrataj_riadky(subor);
 	velkost_pola=pocet_riadkov/6*8+1;
 	pole_SPZ = (char*) malloc(velkost_pola*sizeof(char));
+	
 	riadok=0;
 	poradie_znaku=-1;
 	rewind(subor);
@@ -244,21 +244,13 @@ void spz (char*pole_SPZ,FILE *subor)
 {
 	int pocet_riadkov=1,pocet_zaznamov,poradie_znaku,pocet_znakov;
 	char znak;
-	rewind(subor);
-	do{
-		do{
-			znak =getc(subor);
-  		
-			if(znak ==EOF)  
-				break;             
-		}while (znak!=('\n')) ;
-	pocet_riadkov++;
-	}while (znak!=EOF);
+	
+	pocet_riadkov=zrataj_riadky(subor);
 	pocet_zaznamov=pocet_riadkov/6;
 	pocet_znakov=pocet_zaznamov*8;
 	for(poradie_znaku=0;poradie_znaku<pocet_znakov;poradie_znaku++)
 	{
-	printf("%c",*(pole_SPZ+poradie_znaku));																			//4 funkcia
+	printf("%c",*(pole_SPZ+poradie_znaku));																			
 	if(poradie_znaku%8==1)
 	printf(" ");
 	if(poradie_znaku%8==4)
@@ -270,19 +262,10 @@ void palindrom(char*pole_SPZ,FILE*subor)
 {
 	int pocet_riadkov=1,pocet_zaznamov,poradie_zaznamu;
 	char znak;
-	rewind(subor);
 	
-	do
-	{
-		do
-		{
-			znak =getc(subor);
-			if(znak ==EOF)  
-				break;             
-		}while (znak!=('\n')) ;
-		pocet_riadkov++;
-	}while (znak!=EOF);
+	pocet_riadkov=zrataj_riadky(subor);
 	pocet_zaznamov=pocet_riadkov/6;
+	
 	for(poradie_zaznamu=0;poradie_zaznamu<pocet_zaznamov;poradie_zaznamu++)
 	{
 	if((*(pole_SPZ+poradie_zaznamu*8))==(*(pole_SPZ+poradie_zaznamu*8+6))&&(*(pole_SPZ+poradie_zaznamu*8+1))==(*(pole_SPZ+poradie_zaznamu*8+5))&&(*(pole_SPZ+poradie_zaznamu*8+2))==(*(pole_SPZ+poradie_zaznamu*8+4)))			// 5 funkcia
@@ -295,18 +278,8 @@ void pocet (char*pole_SPZ,FILE*subor)
 	int pocet_riadkov=1,velkost_histogramu,pocet_zaznamov,pocet_prv_his,poradie_SPZ_v_poli,poradie_max_prvku_v_his,poradie_SPZ_histogram,poradie_prv_v_hist,max;
 	char znak;
 	char *histogram;
-	rewind(subor);
 	
-	do
-	{
-		do
-		{
-			znak =getc(subor);
-			if(znak ==EOF)  
-				break;             
-		}while (znak!=('\n')) ;
-		pocet_riadkov++;
-	}while (znak!=EOF);
+	pocet_riadkov=zrataj_riadky(subor);
 	pocet_zaznamov=pocet_riadkov/6;
 	velkost_histogramu=pocet_zaznamov*3;
 	histogram=(char*) malloc(velkost_histogramu*sizeof(char));
@@ -373,7 +346,7 @@ int main()
  			case 'n':if(otvoreny_subor==1) {pole_SPZ=nacitaj(subor);vytvorene_pole=1;}break;
  			case 's':if(vytvorene_pole==1) spz(pole_SPZ,subor);else printf("pole nie je vytvorene \n");break;
  			case 'p':if(vytvorene_pole==1) palindrom(pole_SPZ,subor);else printf("pole nie je vytvorene \n");break;
- 			case 'z':if(vytvorene_pole==1) pocet(pole_SPZ,subor);else printf("pole nie je vytvorene \n");break;													//funguje tak ako ma
+ 			case 'z':if(vytvorene_pole==1) pocet(pole_SPZ,subor);else printf("pole nie je vytvorene \n");break;													
  			default:break;
  		}
 	}while (znak!='k');
